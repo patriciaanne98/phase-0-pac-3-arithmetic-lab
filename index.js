4dea3c5db860c439760a355eaab0362eb9a12c38
Patricia Frogoso <pfrogoso@gmail.com>
2023-10-17 20:36:48 -0500

    complete lab

---
+++ index.js
@@ -0,0 +1,36 @@
+1 + 80; //=> 81
+60 - 40; //=> 20
+2 * 3.4; //=> 6.8
+5.0 / 2.5; //=> 2
+function add(a,b) {
+    return a + b;
+  }
+function subtract(a,b) {
+    return a - b;
+}
+function multiply (a,b) {
+    return a * b;
+}
+function divide (a,b) {
+    return a / b;
+}
+let number = 5;
+
+function increment(n) {
+    return ++n;
+  }
+function decrement(n) {
+    return --n;
+}
+const n='0x2328'
+
+console.log(n)
+console.log(parseInt(n,10));
+console.log(n)
+function makeInt(n) {
+    return parseInt(n,10)
+}
+function preserveDecimal(n) {
+    return parseFloat(n)
+}
+
